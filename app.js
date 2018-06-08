ddf33d06bfecf3e5f2c123b76fb35b7e734790f0
Ian Schwartz <hankpiece@gmail.com>
2018-06-08 16:01:40 +0000

    Added heatmap functionality and refactored a bunch

--- app.js
+++ app.js
@@ -1,40 +1,36 @@
 /*
-	Originally I wanted to solve this problem using React but
-	I couldn't figure out how to integrate the map tooling with React 
-	without spending a lot of time on it. In the end, I decided to do
-	it with vanilla JS and DOM manipulation, which is slow, but works.
+	Assumptions:
+		0: Not going to spend much time on styling to stay within the 
+			time constraints of the exercise
+		1: The exercise said to use the framework of your choice. I chose MapBox
+			which is a mapping library built on top of Leaflet.js. Leaflet has it's
+			own state management, and attempting to use React was difficult because
+			React's state management works to minimize side effects. In the end I chose
+			to use vanilla JS.
 */
 
 const token = "pk.eyJ1IjoiaWFuc2Nod2FydHoiLCJhIjoiY2ppM2p5MHpvMW11bjNrbncwb2JhdndwcCJ9.RGFucrA0HIuQo5Z988V-wg";
 L.mapbox.accessToken = token;
+
+// Creates a street map and appends it to div#map
 let map = L.mapbox.map('map', 'mapbox.streets');
 let featureLayer = L.mapbox.featureLayer().addTo(map);
-let activeLayer;
 
-// helper methodivs
-function fc(array) {
-	// The geoJSON data received from the wheel is in the Long/Lat format
-	// , but Mapbox expects Lat/Long. This function reverses them.
-	return [array[1], array[0]];
-}
+/*
+	The Trip object contains all the leaflet layers for each geojson file imported 
+		by getMapData(). References to these are stored in an array object for easy manipulation.
+*/
 
-function roundToTwoDecimals(num) {
-	return Math.round(num * 100) / 100;
-}
+let trips = [];
 
-function formateTimeStamp(time) {
-	let newTime = new Date(time);
-	return newTime.toLocaleString();
-}
+function Trip(geojson, url) {
+	this.geojson = geojson;
+	this.tripID = url;
 
-function centerMap(data) {
-	let center = fc(data.features[0].geometry.coordinates);
-	map.setView(center, 17);
-}
-
-function placeFeatures(features) {
-	activeLayer = L.geoJSON(features).addTo(map);
-	activeLayer.eachLayer((layer) => {
+	// on creation, Trip() will add itself to the map by default.
+	centerMap(geojson);
+	this.featureLayer = L.geoJSON(geojson);
+	this.featureLayer.eachLayer((layer) => {
 		let properties = layer.feature.properties
 		let content = `
 			<h2>${formateTimeStamp(properties.time)}</h2>
@@ -42,42 +38,120 @@ function placeFeatures(features) {
 				<li>Speed: ${roundToTwoDecimals(properties.wheelData.speed)}kph</l1>
 				<li>Altitude ${roundToTwoDecimals(properties.sensorData.phone.altitude)}meters</li>
 				<li>Energy Efficiency ${roundToTwoDecimals(properties.wheelData.energyEfficiency)}W*h/km</li>
-			<li>Battery Charge: ${roundToTwoDecimals(properties.wheelData.tripEnergyEfficiency)}%</li>
+				<li>Motor Temperature ${properties.wheelData.motorTemperature}°C</li>
+				<li>Battery Charge: ${roundToTwoDecimals(properties.wheelData.tripEnergyEfficiency)}%</li>
 			</ul>
 		`;
 		layer.bindPopup(content);
-	})
+	});
+
+	// using the plugin leaflet.heat to render the heatmaps
+	function mapProperty(property) { // helper function to format data for leaflet.heat
+		return geojson.features.map((feature) => {
+			return [...fc(feature.geometry.coordinates), feature.properties.wheelData[property]/100]
+		})
+	}
+
+	this.temperatureHeatMapLayer = L.heatLayer(mapProperty("motorTemperature"), {gradient: {0.0: 'black', 1.0: 'red'}});
+	this.torqueLayer = L.heatLayer(mapProperty("riderTorque"), {
+		radius: 40,
+		gradient: {0.0: 'black', 0.5: 'green', 1.0: 'lightgreen',
+	}});
+
+	this.layers = [this.featureLayer, this.temperatureHeatMapLayer, this.torqueLayer];
+}
+
+// helper methods
+function fc(array) {
+	// The geoJSON data received from the wheel is in the Long/Lat format
+	// , but Mapbox expects Lat/Long. This function reverses them.
+	return [array[1], array[0]];
+}
+
+function roundToTwoDecimals(num) { // the wheel gives very precise numbers... Too precise for humans
+	return Math.round(num * 100) / 100;
+}
+
+function formateTimeStamp(time) { // makes timestamps more readable for humans
+	let newTime = new Date(time);
+	return newTime.toLocaleString();
+}
+
+// The following functions interact with the state of our Map object.
+// RIght now they rely mostly on side effects, which is unfortunate
+// But since Leaflet has its own state management, it was easier
+// this way than to use more functional concepts.
+
+function centerMap(data) { // centers the map on the first feature of the geojson
+	let center = fc(data.features[0].geometry.coordinates);
+	map.setView(center, 17);
 }
 
 function clearFeatures() {
-	map.removeLayer(activeLayer)
-}
-
-function updateStatistics(features) {
-	let filteredFeatures = features.filter((feature) => {
-		return feature.properties.wheelData.tripAverageSpeed;
+	trips.forEach((trip) => {
+		trip.layers.forEach((layer) => {
+			map.removeLayer(layer);
+		})
 	})
-	let lastFeature = filteredFeatures[filteredFeatures.length-1];
-	let statsList = document.getElementById('stats');
-	statsList.innerHTML = `
-		<li>Average Speed: ${roundToTwoDecimals(lastFeature.properties.wheelData.tripAverageSpeed)}kph</li>
-		<li>Distance Traveled: ${roundToTwoDecimals(lastFeature.properties.wheelData.tripOdometer)}km</li>
-		<li>Trip Energy Efficiency: ${roundToTwoDecimals(lastFeature.properties.wheelData.tripEnergyEfficiency)}W*h/km</li>
-	`
-
 }
 
+function activateLayer(index, layer) {
+	let myTrip = trips[index];
+	let myLayer = myTrip[layer];
+	centerMap(myTrip.geojson);
+	myLayer.addTo(map);
+}
+
+function removeLayer(index, layer) {
+	let myTrip = trips[index];
+	let myLayer = myTrip[layer];
+	map.removeLayer(myLayer);
+}
+
+// 
+function listTrips() {
+	let list = trips.reduce((string, trip, index) => {
+		return string += `<li>
+			<h4>${trip.tripID}</h4>
+			<div>
+				<button class="list-control" onclick="activateLayer(${index}, 'featureLayer')">Show DataPoints</button>
+			</div>
+			<div>
+				<button class="list-control" onclick="removeLayer(${index}, 'featureLayer')">Remove DataPoints</button>
+			</div>
+			<div>
+				<button class="list-control" onclick="activateLayer(${index}, 'temperatureHeatMapLayer')">Show Motor Temperature</button>
+			</div>
+			<div>
+				<button class="list-control" onclick="removeLayer(${index}, 'temperatureHeatMapLayer')">Remove Motor Temperature</button>
+			</div>
+			<div>
+				<button class="list-control" onclick="activateLayer(${index}, 'torqueLayer')">Show Torque </button>
+			</div>
+			<div>
+				<button class="list-control" onclick="removeLayer(${index}, 'torqueLayer')">Remove Torque</button>
+		</div></
+		li>`
+	}, "");
+	let ul = document.getElementById('trips');
+	ul.innerHTML = list;
+}
+
+// at the moment, the URLs are hardcoded into the HTML onclick function
+//	that calls getMapData(). Ideally, I'd be calling an api with a list of 
+//	trips and getting the URL's for each geojson file dynamically through API calls
+//	
 function getMapData(url) {
 	console.log('fetching');
 	fetch(url).then((res) => {
 		console.log('fetch successful');
 		return res.json();
 	}).then((data) => {
-		console.log('centering map');
-		centerMap(data);
-		return data;
-	}).then((features) => {
-		placeFeatures(features);
-		updateStatistics(features.features);
+		clearFeatures();
+		let trip = new Trip(data, url);
+		let id = trips.length;
+		trips.push(trip);
+		activateLayer(id, "featureLayer");
+		listTrips();
 	}).catch(error => console.log('Error:', error))
 }
